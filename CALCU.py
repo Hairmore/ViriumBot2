709fb98b0018523d04aebad70e34c11b0dfa290d
Hairmore <85640694+Hairmore@users.noreply.github.com>
2022-05-10 04:25:30 +0200

    Update CALCU.py

--- CALCU.py
+++ CALCU.py
@@ -209,6 +209,58 @@ async def SCORE(cmd):
         updated_data.to_csv(csv_file, index=False, sep='\t')      
     
     else:
+        try:
+            print(counter, num_quota)
+        except NameError:
+            await cmd.send(f"{cmd.guild.get_role(TICKET_ADMIN_ROLE_ID).mention}, Please inform the bot how many whitelist will be given with command `/whitelist_number`.")
+        ##########################
+        members = cmd.guild.members
+        """Count how many invites the command giver has"""
+        csv_invite = pd.read_csv(
+            os.getcwd() + '/database_inviter.csv', 
+            sep="\t",
+            dtype = {'user_id': str,'inviter_id':str}
+        ) 
+        try:
+            invite_times = csv_invite['inviter_id'].value_counts()[str(cmd.author.id)]
+        except KeyError:
+            invite_times = 0
+        """Compare it with existing data"""
+        existing_data = pd.read_csv(csv_file, sep="\t", dtype = {'mem_id': str})
+        existing_ids = [id for id in existing_data["mem_id"].tolist()]
+        for member in members:
+            if not member.bot:
+                team_role = [936964587717263400, 935826124368404500, 947164819927236648, 935827096373186630]#origine admin senior junior
+                roles = [r.id for r in member.roles]
+                inter_set = list(set(team_role)&set(roles))
+                if len(inter_set) == 0:
+                    dic_data = {}
+                    if str(member.id) not in existing_ids:
+                        #If someone new enters our server
+                        dic_data["mem_id"] = str(member.id)
+                        dic_data["mem_name"] = member.name
+                        dic_data["roles"] = [str(role.id) for role in member.roles]
+                        dic_data["eventEngageTimes"] = 0
+                        dic_data["botUseTimes"] = 0
+                        dic_data["spamTimes"] = 0
+                        dic_data["MisbehaviorTimes"] = 0
+                        dic_data["Invites"] = invite_times
+                        dic_data["Followed"] = "No"
+                        dic_data["InfluencerFollower"] = 0
+                        dic_data["FUD"] = 0              
+                        dic_data["BadSub"] = 0    
+                        dic_data["spamRepo"] = 0               
+                        dic_data["score"] = 0
+                        dic_data["roles_name"] = [role.name for role in member.roles]
+                        existing_data = existing_data.append([dic_data], ignore_index=True)
+                    else:
+                        #We update current info
+                        index_ = existing_ids.index(str(member.id))
+                        existing_data.iloc[index_, 2] = str(list([role.id for role in member.roles]))
+                        existing_data.iloc[index_, 7] = invite_times
+                        existing_data.iloc[index_, 14] = [role.name for role in member.roles]
+                        
+        existing_data.to_csv(csv_file, index=False, sep='\t')                
         await cmd.send(f"Whitelist round {this_round} ended.")
         updated_data = pd.read_csv(csv_file, sep="\t", dtype = {'mem_id': str})
     
