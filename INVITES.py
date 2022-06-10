8b1196b85b9320197d6e865c55e845979440b749
Hairmore <85640694+Hairmore@users.noreply.github.com>
2022-06-10 16:38:42 +0200

    invites error

--- INVITES.py
+++ INVITES.py
@@ -22,9 +22,13 @@ if not os.path.exists(csv_file):
 
 @com.event
 async def on_member_remove(member):
+    print(1, "remove starts")
     df_leave = pd.read_csv(csv_file, sep="\t", dtype = {'user_id': str,'inviter_id':str})
+    print(2, df_leave)
     leave_index = df_leave[df_leave['user_id'] == member.id].index.tolist()
+    print(3, leave_index)
     df_leave = df_leave.drop(df_leave.index[[leave_index]])
+    print(4, "minus", df_leave)
     df_leave.to_csv(csv_file, sep="\t", index=False)
 
  
@@ -32,13 +36,17 @@ async def on_member_remove(member):
 async def on_member_join(member):
     if not member.bot:
         inviter = await tracker.fetch_inviter(member)
+        print(5, "reack start")
         dic_inv_info = {}
         dic_inv_info['user_name'] = member.name
         dic_inv_info['user_id'] = str(member.id)
         dic_inv_info['inviter_name'] = inviter.name
         dic_inv_info['inviter_id'] = str(inviter.id)
         df_adding = pd.read_csv(csv_file, sep="\t", dtype = {'user_id': str,'inviter_id':str})
-        df_adding = df_adding.append([dic_inv_info], ignore_index=True)
+        df_new_row = pd.DataFrame(dic_inv_info)
+        print(6, "new line", df_new_row)
+        df_adding = pd.concat([df_adding, df_new_row])
+        print(7, "new data", df_adding)
         df_adding.to_csv(csv_file, sep="\t", index=False)
 
 
